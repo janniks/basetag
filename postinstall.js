875f645eadee56cd29e3bb5ee08dd313a97e20b9
janniks <jannik.siebert@gmail.com>
2020-04-19 00:53:37 +0200

    Reorganize exist checks

--- postinstall.js
+++ postinstall.js
@@ -1,39 +1,48 @@
 const fs = require("fs");
 const path = require("path");
 
+const package = require("./package");
+
+// Helpers
 const reset = "\x1b[0m";
 const yellow = "\x1b[33m";
 const blue = "\x1b[34m";
 
-const logBlue = (message) => console.log(`${blue}${message}${reset}`);
-const logYellow = (message) => console.warn(`${yellow}${message}${reset}`);
+const log = (message) => console.log(`${blue}${message}${reset}`);
 
-logBlue("at-base:");
+function fileExists(path) {
+  try {
+    fs.accessSync(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+// Main
+log(`at-base ${package.version}:`);
 
 try {
-  const scriptPath = fs.realpathSync(__dirname);
-
-  logBlue(scriptPath);
-
-  const lIndex = scriptPath.lastIndexOf("/node_modules/");
+  const lIndex = __dirname.lastIndexOf("/node_modules/");
   if (lIndex === -1) {
-    throw "- Could not find node_modules directory in __dirname\n- Not creating @ symlink";
+    throw "- Could not find node_modules directory in __dirname";
   }
 
-  const base = scriptPath.slice(0, lIndex);
-  logBlue(base);
-
+  const base = path.resolve(__dirname.slice(0, lIndex));
   const atLink = path.resolve(base, "node_modules/@");
 
-  logBlue(atLink);
+  if (fileExists(atLink)) {
+    if (base === fs.realpathSync(atLink)) {
+      log("- @ symlink already points to base\n");
+      process.exit();
+    }
 
-  if (fs.accessSync(atLink)) {
-    throw "- File already exists\n- Not creating @ symlink";
+    throw "- File already exists: node_modules/@";
   }
 
   fs.symlinkSync(base, atLink, "junction");
 
-  logBlue(`- Created @ symlink to ${base}\n`);
+  log(`- Created @ symlink to ${base}\n`);
 } catch (error) {
-  logYellow(error);
+  console.warn(`${yellow}${error}\n- Not creating @ symlink${reset}\n`);
 }
