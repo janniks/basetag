b659f0a1a67af045811e71079c5990905c22c5c4
janniks <jannik.siebert@gmail.com>
2020-04-18 23:55:19 +0200

    Update logging

--- postinstall.js
+++ postinstall.js
@@ -6,21 +6,24 @@ const yellow = "\x1b[33m";
 const blue = "\x1b[34m";
 
 const logBlue = (message) => console.log(`${blue}${message}${reset}`);
-const logYellow = (message) => console.log(`⚠️ ${yellow}${message}${reset}`);
+const logYellow = (message) => console.warn(`⚠️ ${yellow}${message}${reset}`);
 
 logBlue("at-base:");
-logBlue("- Creating @ symlink to base directory...");
 
 try {
   const scriptPath = fs.realpathSync(__dirname);
 
   const lIndex = scriptPath.lastIndexOf("/node_modules/");
   if (lIndex === -1) {
-    throw "- E010: Could not find node_modules directory in __dirname";
+    throw "- Could not find node_modules directory in __dirname\n- Not creating @ symlink";
   }
 
   const base = scriptPath.slice(0, lIndex);
   const atLink = path.resolve(base, "node_modules/@");
+  if (fs.existsSync(path)) {
+    throw "- File already exists\n- Not creating @ symlink";
+  }
+
   fs.symlinkSync(base, atLink, "junction");
 
   logBlue(`- Created @ symlink to ${base}\n`);
